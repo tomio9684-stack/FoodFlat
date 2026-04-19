a10580cf7c7d2bed789e96ba76dcd7a13caf9c32
tomio9684-stack <tomio9684@gmail.com>
2026-04-19 16:17:16 +0100

    Update server.js

--- server.js
+++ server.js
@@ -167,4 +167,6 @@ res.json({ price: deliveryPrice });
 
 app.get(”/delivery-price”, (req, res) => res.json({ price: deliveryPrice }));
 
-app.listen(3000, () => console.log(“Foodflat running on port 3000”));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Foodflat running on port " + PORT));
+
